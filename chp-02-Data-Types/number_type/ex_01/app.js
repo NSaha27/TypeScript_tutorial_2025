b2f15f59f948127cfd659561a85b7644722cb779
NSaha27 <sahaniladri27y@gmail.com>
2025-05-22 20:07:20 +0530

    examples on string data types, type inference, and type conversion to a string

---
+++ chp-02-Data-Types/number_type/ex_01/app.js
@@ -0,0 +1,9 @@
+// Number Type
+var num1 = 25;
+var num2 = 38;
+var res = num1 + num2;
+console.log(res);
+// res = "Coding step by step";
+var num3 = 42;
+res += num3;
+console.log(res);
---
+++ chp-02-Data-Types/number_type/ex_01/app.ts
@@ -0,0 +1,13 @@
+// Number Type
+
+let num1: number = 25;
+let num2: number = 38;
+
+let res = num1 + num2;
+console.log(res);
+
+// res = "Coding step by step";
+
+let num3: number = 42;
+res += num3;
+console.log(res);
