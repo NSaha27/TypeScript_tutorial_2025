8a2584631d9943aaebb4e13e2934e4df5dd0597a
NSaha27 <sahaniladri27y@gmail.com>
2025-07-23 20:18:58 +0530

    tuples- examples on tuples in typescript

---
+++ chp-02-Data-Types/tuple_type/examples/ex-01/app.js
@@ -0,0 +1,9 @@
+/*
+*********************************
+  TUPLES IN TYPESCRIPT
+*********************************
+*/
+var user1 = [101, "Arnab Roy", "+919830532144", "arnab.roy26@gmail.com"];
+console.log(user1, typeof user1);
+var user2 = [102, "Niladri Saha", "8420520492", "niladri.saha26@gmail.com", true];
+console.log(user2, typeof user2);
---
+++ chp-02-Data-Types/tuple_type/examples/ex-01/app.ts
@@ -0,0 +1,11 @@
+/*
+*********************************
+  TUPLES IN TYPESCRIPT
+*********************************
+*/
+
+const user1: [ID: number, name: string, phone: string, email: string] = [101, "Arnab Roy", "+919830532144", "arnab.roy26@gmail.com"];
+console.log(user1, typeof user1);
+
+const user2: [number, string, string, string, boolean] = [102, "Niladri Saha", "8420520492", "niladri.saha26@gmail.com", true];
+console.log(user2, typeof user2);
