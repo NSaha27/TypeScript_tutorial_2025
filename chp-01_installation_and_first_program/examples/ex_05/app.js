aba330897db01450fbef382e7d004d1ed2f6cf40
Niladri Saha <sahaniladri27y@gmail.com>
2026-01-19 21:02:57 +0530

    examples on number data type

---
+++ chp-01_installation_and_first_program/examples/ex_05/app.js
@@ -0,0 +1,33 @@
+/*
+  PRODUCT DETAILS
+*/
+var products = [
+    {
+        pBNo: "2504102",
+        pName: "Boat Airdops 138",
+        pType: "Headphones & Earbuds",
+        pBrand: "Boat",
+        pMkd: "May, 2025",
+        pExp: 1,
+        pPrice: 1999.0,
+    },
+    {
+        pBNo: "4210539",
+        pName: "Moto G84 5G",
+        pType: "Mobiles",
+        pBrand: "Motorola",
+        pMkd: "Oct, 2025",
+        pExp: 1,
+        pPrice: 18999.0,
+    },
+    {
+        pBNo: "805321579",
+        pName: "Dell 14 Plus Next Gen AI PC",
+        pType: "Laptops & Notebooks",
+        pBrand: "Dell",
+        pMkd: "Dec, 2025",
+        pExp: 1,
+        pPrice: 74990.0,
+    },
+];
+console.table(products);
---
+++ chp-02-Data-Types/number_type/ex_02/app.js
@@ -0,0 +1,41 @@
+/*
+  NUMBER DATA TYPE
+  - declare a number data type variable and use it
+  - type inference
+  - other data type to number data type conversion
+*/
+/*
+// declare number data type variables and error detection
+const prodID: number = 101;
+let prodPrice: number = 125.8;
+// let discountRate: number = "20"; // will throw a typeError
+let discountRate: number = 20;
+let taxRate: number = 12.02;
+
+const discount = (prodPrice * discountRate) / 100;
+const tax = ((prodPrice - discount) * taxRate) / 100;
+
+console.log(`Product details:
+------------------------------------
+Product ID:            ${prodID}
+------------------------------------
+
+Gross Amt($):          ${prodPrice}
+Discount($):        (-)${Math.round(discount)}
+Tax($):             (+)${Math.round(tax)}
+------------------------------------
+Net Amt($) (approx.):  ${Math.round(prodPrice - discount + tax)}
+------------------------------------
+`);
+*/
+/*
+// type inference (auto type detection)
+const num = 52;
+const num2 = 17;
+const total = num + num2;
+console.log(total);
+*/
+// type conversion
+var prodID = Number("123");
+var isProdVerified = Number(true);
+console.log(prodID, isProdVerified);
