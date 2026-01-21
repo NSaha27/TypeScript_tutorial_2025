fff9c3fcf7e676ff3d4ec61317e8b03bd34a4eed
Niladri Saha <sahaniladri27y@gmail.com>
2026-01-21 21:03:52 +0530

    create, configure a tsconfig.json file, and compile all .ts file at the same time and save them in a seperate folder

--- chp-03-use_of_tsconfig_file/examples/ex-03-generate_tsconfig_file/output/app.js
+++ chp-03-use_of_tsconfig_file/examples/ex-03-generate_tsconfig_file/output/app.js
@@ -8,5 +8,5 @@ console.log(`Dear ${typeof uname === "string" ? uname : "admin"}, you're ${curAg
   Note - To compile all the .ts files at the same time,
   - first initialize a .tsconfig file by typing "npx tsc --init" in the terminal and on the same project folder,
   - then configure the .tsconfig file as per your need,
-  - then type "tsc" in the terminal to compile all the files at the same time.
+  - then type "npx tsc" in the terminal to compile all the files at the same time.
 */
